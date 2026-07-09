4d5d67f059d8845ade6a4571069c73a29b5a7e45
DenysHandziichuk <fleymidesign@gmail.com>
2026-07-09 04:40:02 -0400

    refactor(store): delete unused store/index.ts indexer file

--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-export { useChatStore } from "./useChatStore";
-export { useModelStore } from "./useModelStore";
-export { useUIStore } from "./useUIStore";
-export { useSearchStore } from "./useSearchStore";
-export { useMcpStore } from "./useMcpStore";
-export { useProjectStore } from "./useProjectStore";
-export { useKeybindStore } from "./useKeybindStore";
-export * from "./helpers";
